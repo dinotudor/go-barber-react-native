77864e73c9289d0ee2fe611425bb7dfca2708550
Dino Tudor <dinotudor@gmail.com>
2019-10-18 13:17:08 +0200

    add input and button component, vector icons dep, manual link

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
-
 import Background from '../../components/Background';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SIGN IN</Text>
+      <Input style={{ marginTop: 30 }} icon="email" placeholder="name" />
+      <Button>
+        <Text>Sign In</Text>
+      </Button>
     </Background>
   );
 }
