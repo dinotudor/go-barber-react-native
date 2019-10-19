fe31c23ff98c1e1f83d909fe1461790e007dc931
Dino Tudor <dinotudor@gmail.com>
2019-10-19 15:40:24 +0200

    status bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import Routes from './routes';
 
-export default function src() {
-  return <Routes />;
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="light-content" background="#7159c1" />
+      <Routes />
+    </>
+  );
 }
