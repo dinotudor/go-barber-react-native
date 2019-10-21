6c10ac085b85265b0818e8ce66f28b02e95f8de1
Dino Tudor <dinotudor@gmail.com>
2019-10-21 18:44:50 +0200

    createBottomTabNavigator, react-reanimated, App.js and Index.js to check login

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ import './config/ReactotronConfig';
 
 import { store, persistor } from './store';
 
-import Routes from './routes';
+import App from './App';
 
-export default function App() {
+export default function Index() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <StatusBar barStyle="light-content" background="#7159c1" />
-        <Routes />
+        <App />
       </PersistGate>
     </Provider>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,25 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SingUp';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
-);
+import Dashboard from './pages/Dashboard';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
