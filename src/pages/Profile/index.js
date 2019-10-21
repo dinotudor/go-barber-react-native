8651fc226dad121996b8b42e0f47e8e4c0b29c10
Dino Tudor <dinotudor@gmail.com>
2019-10-21 20:26:32 +0200

    Pages, Dashboard, Profile, load background and customize bottomTabNavigator

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
-
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/Background';
 
 // import { Container } from './styles';
 
-export default function Dashboard() {
+export default function Profile() {
   return <Background />;
 }
 
-Dashboard.navigationOptions = {
-  tabBarLabel: 'Appointments',
+Profile.navigationOptions = {
+  tabBarLabel: 'My Profile',
   tabBarIcon: ({ tintColor }) => (
-    <Icon name="event" size={20} color={tintColor} />
+    <Icon name="person" size={20} color={tintColor} />
   ),
 };
