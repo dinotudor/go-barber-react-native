d48ca16aff9902f94f8b7c08ac4e504e6f3bd336
Dino Tudor <dinotudor@gmail.com>
2019-11-02 18:09:07 +0100

    Profile page WIP

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,14 +1,96 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/Background';
 
-import { Container } from './styles';
+import {
+  Container,
+  Title,
+  Separator,
+  Form,
+  FormInput,
+  SubmitButton,
+} from './styles';
+// import { FormInput, SubmitButton } from '../SignIn/styles';
 
 export default function Profile() {
+  const emailRef = useRef();
+  const oldPasswordRef = useRef();
+  const passwordRef = useRef();
+  const confirmPasswordRef = useRef();
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [oldPassword, setOldPassword] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  function handleSubmit() {}
+
   return (
     <Background>
-      <Container />
+      <Container>
+        <Title>My profile</Title>
+        <Form>
+          <FormInput
+            icon="person-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Your name"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
+          />
+          <FormInput
+            icon="mail-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            keyboardType="email-address"
+            placeholder="Type your email"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => oldPasswordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+          <Separator />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Current Password"
+            ref={oldPasswordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => confirmPasswordRef.current.focus()}
+            value={oldPassword}
+            onChangeText={setOldPassword}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Type new password"
+            ref={passwordRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Confirm password"
+            ref={confirmPasswordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
+            value={confirmPassword}
+            onChangeText={setConfirmPassword}
+          />
+
+          <SubmitButton onPress={handleSubmit}>Update Profile</SubmitButton>
+        </Form>
+      </Container>
     </Background>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -1,3 +1,35 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  color: #fff;
+  font-weight: bold;
+  align-self: center;
+  margin-top: 30px;
+`;
+
+export const Separator = styled.View`
+  height: 1px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 20px 0 30px;
+`;
+
+export const Form = styled.View`
+  align-self: stretch;
+  margin-top: 50px;
+`;
+
+export const FormInput = styled(Input)`
+  margin-bottom: 10px;
+`;
+
+export const SubmitButton = styled(Button)`
+  margin-top: 5px;
+`;
