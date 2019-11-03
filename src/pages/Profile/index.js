c37b8ba131bf279b0b9f936e4c2d2cb44aa29192
Dino Tudor <dinotudor@gmail.com>
2019-11-03 12:04:41 +0100

    logout

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Background from '../../components/Background';
+import { signOut } from '../../store/modules/auth/actions';
 import { updateProfileRequest } from '../../store/modules/user/actions';
 
 import {
@@ -12,6 +13,7 @@ import {
   Form,
   FormInput,
   SubmitButton,
+  LogoutButton,
 } from './styles';
 
 export default function Profile() {
@@ -45,6 +47,10 @@ export default function Profile() {
         confirmPassword,
       })
     );
+  }
+
+  function handleLogout() {
+    dispatch(signOut());
   }
 
   return (
@@ -109,6 +115,7 @@ export default function Profile() {
           />
 
           <SubmitButton onPress={handleSubmit}>Update Profile</SubmitButton>
+          <LogoutButton onPress={handleLogout}>Log Out</LogoutButton>
         </Form>
       </Container>
     </Background>
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -36,3 +36,8 @@ export const FormInput = styled(Input)`
 export const SubmitButton = styled(Button)`
   margin-top: 5px;
 `;
+
+export const LogoutButton = styled(Button)`
+  margin-top: 5px;
+  background: #f64c75;
+`;
