2e388530654ae5cc7f64abf283b7031dfcd56f64
Dino Tudor <dinotudor@gmail.com>
2019-11-12 11:27:03 +0100

    hook use State for listing available time

--- src/pages/New/SelectDateTime/index.js
+++ src/pages/New/SelectDateTime/index.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import api from '../../../services/api';
 
 import Background from '../../../components/Background';
 import DateInput from '../../../components/DateInput';
 
 import { Container } from './styles';
 
-export default function SelectDateTime() {
+export default function SelectDateTime({ navigation }) {
   const [date, setDate] = useState(new Date());
+  const [hours, setHours] = useState([]);
+
+  const provider = navigation.getParam('provider');
+
+  useEffect(() => {
+    async function loadAvailable() {
+      const response = await api.get(`providers/${provider.id}/available`, {
+        params: {
+          date: date.getTime(),
+        },
+      });
+
+      setHours(response.data);
+    }
+
+    loadAvailable();
+  }, [date, provider.id]);
 
   return (
     <Background>
