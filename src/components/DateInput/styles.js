f8972b096770caedad4527bf273c41914627a5fa
Dino Tudor <dinotudor@gmail.com>
2019-11-12 10:13:53 +0100

    styles to date input WIP

--- src/components/DateInput/styles.js
+++ src/components/DateInput/styles.js
@@ -1,7 +1,21 @@
-import styled from 'styled-components';
+import styled from 'styled-components/native';
 
-export const Container = styled.div``;
+export const Container = styled.View`
+  margin: 60px 0 30px;
+`;
 
-export const DateButton = styled.div``;
+export const DateButton = styled.TouchableOpacity`
+  padding: 0 15px;
+  height: 46px;
+  background: rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  margin: 0 30px;
+  flex-direction: row;
+  align-items: center;
+`;
 
-export const DateText = styled.div``;
+export const DateText = styled.Text`
+  font-size: 14px;
+  color: #fff;
+  margin-left: 15px;
+`;
