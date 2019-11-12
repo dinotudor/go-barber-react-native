3526c69e3a35084422d0cf64f01e40fa9945f67e
Dino Tudor <dinotudor@gmail.com>
2019-11-12 13:02:40 +0100

    confirm done, routes resetOnBlur

--- src/routes.js
+++ src/routes.js
@@ -57,6 +57,7 @@ export default (isSigned = false) =>
             Profile,
           },
           {
+            resetOnBlur: true,
             tabBarOptions: {
               keyboardHidesTabBar: true,
               activeTintColor: '#FFF',
